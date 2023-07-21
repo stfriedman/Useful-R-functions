9d011cd6d41e883cf3b539cb2c4b09d13716e5d9
Sarah Friedman <sarahtfried@gmail.com>
2023-07-21 15:28:17 -0700

    Update pic_test.R

--- pic_test.R
+++ pic_test.R
@@ -35,6 +35,7 @@ pic_test <- function(data, tree, tip_states, anc = TRUE, ..){
   
   if(any(class(tree) == "multiPhylo")){
     n <- length(tree)
+    tree <- structure(tree, class = c("list","multiSimmap","multiPhylo"))
     # getting mapped edge from each simmap
     sim_tib <- tibble(sim_id = 1:n) %>%
       mutate(simmap = tree,
